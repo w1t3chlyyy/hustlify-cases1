832c2f1710a4e5bb19744b252b9860c623acc123
w1t3chlyyy <w1t3chlyyy@mail.ru>
2026-07-31 22:41:33 +0300

    Update broadcast.ts

--- src/lib/bot/broadcast.ts
+++ src/lib/bot/broadcast.ts
@@ -52,7 +52,7 @@ export async function broadcastCasePublished(c: CaseItem): Promise<void> {
 
   const kb = new InlineKeyboard();
   if (siteUrl) {
-    kb.url("👀 Смотреть кейс", `${siteUrl}/cases/${c.slug}`);
+    kb.url("👀 Смотреть кейс", `${siteUrl}/app`);
   }
 
   for (const id of ids) {
